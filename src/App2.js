f84c45d14e8566c795900b841447ba94dba0ba7a
Amna <amnar23498@gmail.com>
2020-08-30 20:01:26 +0500

    Component Learning

---
+++ src/App2.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function App2()
+{
+    return <div>Hello World from App2.js</div>
+}
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import App2 from './App2'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><p>Let's Start</p><br></br><App></App></div>,
   document.getElementById('root')
 );
-
+ReactDOM.render(
+  <App2></App2>,
+  document.getElementById('root1')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
